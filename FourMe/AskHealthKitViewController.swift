e93f7da777cf90a0503d607e734fc84f33f2f096
Lars Klassen <larcho@gmail.com>
2019-10-26 19:50:29 -0700

    Splash Screen, App Icon and other stuff

--- FourMe/AskHealthKitViewController.swift
+++ FourMe/AskHealthKitViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignupViewController.swift
+//  AskHealthKitViewController.swift
 //  FourMe
 //
 //  Created by Lars Klassen on 10/26/19.
@@ -7,26 +7,16 @@
 //
 
 import UIKit
-import HealthKit
 
-class SignupViewController: UIViewController {
+class AskHealthKitViewController: UIViewController {
 
-    let healthStore = HKHealthStore();
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let allTypes = Set([HKObjectType.quantityType(forIdentifier: .height)!,
-                            HKObjectType.quantityType(forIdentifier: .bodyMass)!,
-                            HKObjectType.characteristicType(forIdentifier: .biologicalSex)!]);
-        
-        healthStore.requestAuthorization(toShare: nil, read: allTypes) { (success, error) in
-            
-        }
-        
+
         // Do any additional setup after loading the view.
     }
-  
+    
+
     /*
     // MARK: - Navigation
 
